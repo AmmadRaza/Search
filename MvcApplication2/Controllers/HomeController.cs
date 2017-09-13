c868c21f29547cb9cb0888757593c394514fcdbb
AmmadRaza <ammadraza001@gmail.com>
2017-09-13 14:57:30 -0700

    Commit

---
+++ MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        abcEntities db = new abcEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search(string keyword = "")
+        {
+            if (keyword !="")
+            {
+                var products = db.Products.Where(p => p.Name.ToLower().StartsWith(keyword.ToLower()));
+                return Json(products, JsonRequestBehavior.AllowGet);
+            }
+            return Json(null, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
